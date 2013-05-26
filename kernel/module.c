57e6b39f273c19636c03ae3c6f05bec3dd25dcc5
javilonas <admin@lonasdigital.com>
2013-05-26 18:44:13 +0200

    Fix Soporte Exfat

--- kernel/module.c
+++ kernel/module.c
@@ -1023,6 +1023,10 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+	/* Soporte exfat */
+	if(!strncmp("exfat_", mod->name, 6))
+	    return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
